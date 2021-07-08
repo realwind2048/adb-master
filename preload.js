237d10062513e86e8d03a42ab5d5303a9bb47097
Hyunghui Kim <realwind2048@gmail.com>
2021-07-09 02:51:52 +0900

    parseDevice

--- preload.js
+++ preload.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
 function getDevices() {
   console.log("getDevices");
   const { exec } = require("child_process");
-  exec("adb devices", (error, data, getter) => {
+  exec("adb devices -l", (error, data, getter) => {
     if (error) {
       console.log("error  22", error);
       return;
@@ -30,7 +30,6 @@ function getDevices() {
       return;
     }
     console.log("data 33", data);
-    document.querySelector("#result").innerText = data;
     document.querySelector("#log-console").innerText += data;
     saveDevices(data);
   });
@@ -39,12 +38,19 @@ function getDevices() {
 var devices = [];
 function saveDevices(sss) {
   var ks = sss.split(/\r?\n/);
-  ks.forEach((element, index, arr) => {
+  ks.forEach((element, index) => {
     if (index >= 1 && element) {
-      devices.push(element);
+      devices.push(parseDevice(element));
     }
   });
   console.log(devices);
+  // document.querySelector("#result").innerText = devices[0];
+  var list = document.querySelector("#device-list");
+  devices.forEach((e) => {
+    var item = document.createElement('li');
+    item.innerHTML = e.json();
+    list.appendChild(item);
+  })
 }
 
 function printLogs(sss) {
@@ -52,3 +58,32 @@ function printLogs(sss) {
   console.log(ks);
   console.log(ks[0]);
 }
+
+function parseDevice(deviceString) {
+  var ls = deviceString.split(" ");
+  var device = new Device();
+  var index = 0;
+  ls.forEach((element) => {
+    if (element) {
+      if (index == 0) {
+        device.id = element;
+      } else if (element.startsWith("model:")) {
+        var colonIdx = element.indexOf(":");
+        device.model = element.slice(colonIdx + 1);
+      }
+      index++;
+      console.log(element);
+    }
+  });
+  return device;
+}
+
+const Device = class {
+  constructor(id, model) {
+    this.id = id;
+    this.model = model;
+  }
+  json() {
+    return `${this.id} ${this.model}`;
+  }
+};
